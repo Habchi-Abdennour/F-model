1aefdf41c63421ace3cd435f6088b295af08f295
Habchi-Abdennour <164701825+Habchi-Abdennour@users.noreply.github.com>
2024-05-28 10:42:14 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,6 @@
 import pandas as pd
 from prophet import Prophet
 
-# Suppress warning messages
-
 class ProphetForecast:
     def __init__(self, dates, values):
         self.df = pd.DataFrame({'ds': pd.to_datetime(dates), 'y': values})
@@ -15,16 +13,17 @@ class ProphetForecast:
     def fit_model(self):
         self.model.fit(self.df)
     
-    def create_future_dataframe(self, periods=36, freq='MS'):
-        self.future = self.model.make_future_dataframe(periods=periods, freq=freq)
+    def create_future_dataframe(self, periods=24, freq='M'):
+        last_date = pd.to_datetime('2023-12-31')  # Ensure it starts from a specific date
+        future_dates = pd.date_range(start=last_date, periods=periods+1, freq=freq)[1:]
+        self.future = pd.DataFrame({'ds': future_dates})
     
     def make_predictions(self):
         self.forecast = self.model.predict(self.future)
     
     def print_forecast(self, n):
         forecast_dict = {
-            "dates": self.forecast['ds'].tail(n).dt.strftime('%Y-%m-%d').tolist(),
-            "ordered": self.forecast['yhat'].tail(n).tolist()
+            "dates": self.forecast['ds'].head(n).dt.strftime('%Y-%m-%d').tolist(),
+            "ordered": self.forecast['yhat'].head(n).tolist()
         }
-        return(forecast_dict)
-
+        return forecast_dict
